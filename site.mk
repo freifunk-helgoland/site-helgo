56d03086ed602d1992d6ee35e41a7e81a19e736f
bdobe <bdobe@users.noreply.github.com>
2016-11-22 16:10:25 +0100

    site.mk: package site.mk: package gluon-luci-wifi-config added

--- site.mk
+++ site.mk
@@ -14,6 +14,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
+	gluon-luci-wifi-config \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
